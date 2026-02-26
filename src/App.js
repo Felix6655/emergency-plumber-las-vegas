f724782eb1fb4efbc5707acf601021597f42e3d2
Felix6655 <joseleonb48@gmail.com>
2026-02-26 12:34:54 -0800

    Fix preview iframe headers

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Emergency Plumber Las Vegas</h1>
+      <p>Lead-gen landing page with call + quote form coming soon.</p>
+    </div>
+  );
+}
+
+export default App;
